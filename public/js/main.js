9668ff243bae3e2e37e1606e8f476bcb19af2ada
Victor Torres <victor.h.c.torres@gmail.com>
2018-07-20 01:15:02 +0100

    adicionado contadores e campos para digitar texto

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,12 @@ var totalPalavras = frase.split(" ").length;
 
 var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(totalPalavras);
+
+var campoTextArea = $(".campo-digitacao");
+
+campoTextArea.on("input", function(){
+    var conteudo = campoTextArea.val();
+    var totalPalavrasDigitadas = conteudo.split(/\S+/).length; //essa expressao regular busca por qualquer espaco vazio
+    $("#contador-palavras").text(totalPalavrasDigitadas);
+    $("#contador-caracteres").text(conteudo.length);
+});
