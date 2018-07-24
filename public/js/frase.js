f14810290bc95cf1980033d2b4488875794c864c
Victor Torres <victor.h.c.torres@gmail.com>
2018-07-24 20:13:09 +0100

    adicionado spinner para dar um feedback visual para o usuario

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,8 +1,28 @@
 $("#botao-frases").click(selecionarFraseAleatoria);
 
 function selecionarFraseAleatoria(){
+    //poderia usar o .show() e depois o .hide() mas como sempre
+    //terei que ficar fazendo essa transicao o .toggle() eh uma
+    //solucao melhor
+    $("#spinner").toggle();
+    var mensagemErro = $("#erro");
     //para chamar uma requisicao get usando jquery
-    $.get("http://localhost:3000/frases", trocarFrase);
+    $.get("http://localhost:3000/frases", 
+          trocarFrase)
+    //este bloco abaixo eh executado sempre
+    //que ocorrer um erro
+    .fail(function(){
+        mensagemErro.show();
+        setTimeout(function(){
+            mensagemErro.toggle();
+        }, 1500);
+        
+    })
+    //este bloco eh executado sempre apos a execucao de
+    //sucesso ou de erro
+    .always(function(){
+        $("#spinner").toggle();
+    });
 }
 
 function trocarFrase(data){
